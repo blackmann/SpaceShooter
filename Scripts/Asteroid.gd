7b07278acdb98c039833b88f3c581c3950c79b9c
De-Great Yartey <mail@degreat.co.uk>
2023-03-10 13:03:42 +0000

    Glow and explode

--- Scripts/Asteroid.gd
+++ Scripts/Asteroid.gd
@@ -4,10 +4,18 @@ extends Node2D
 @export var rotation_speed: int = 10
 @export var health = 1
 
+var check_emission_done = false
+
 func _process(delta):
-	if health <= 0:
-		queue_free()
+	if health <= 0 and not check_emission_done:
+		$Sprite2D.hide()
+		$GPUParticles2D.emitting = true
+		check_emission_done = true
 		Global.score += 1
+		return
+		
+	if check_emission_done and not $GPUParticles2D.emitting:
+		queue_free()
 		return
 		
 	position += Vector2(0, 1) * delta * speed
@@ -18,6 +26,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_area_2d_area_entered(area: Area2D):
-	if area.is_in_group("rock_destroyer"):
+	if area.is_in_group("rock_destroyer") and health > 0:
 		area.queue_free()
 		health -= 1
